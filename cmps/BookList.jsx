3f72859ae95098c4c0da3ec77ac590ffbabdcb0a
shanybaruch <baruchshany@gmail.com>
2025-09-24 17:25:59 +0300

    Add btns to books cards

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -1,12 +1,17 @@
 import { BookPreview } from './BookPreview.jsx';
 
-export function BookList({books}) {
+export function BookList({ books }) {
     return (
         <ul className="book-list container">
-            {books.map((book, idx) => 
-                <BookPreview book={book} idx={idx} />
+            {books.map((book, idx) =>
+                <li key={book.id}>
+                    <BookPreview book={book} idx={idx} />
+                    <section>
+                        <button>Remove</button>
+                        <button>Details</button>
+                    </section>
+                </li>
             )}
-
         </ul>
     )
 }
