c3c139c393994acfa7111bef4ba3f5ccd22cad68
shanybaruch <baruchshany@gmail.com>
2025-09-26 16:10:04 +0300

    Basic design for home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,7 +3,9 @@ import { UserPreview } from './UserPreview.jsx'
 export function Home() {
     return (
         <section className='home'>
-            <h2>Home</h2>
+            <h2 className='welcome'>WELCOME</h2>
+            <h2 className='title-logo'>Miss Books</h2>
+            {/* <h2>Home</h2> */}
             {/* <UserPreview /> */}
         </section>
     )
