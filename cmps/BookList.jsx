6dd7d9ab143d09b7a52ec630d474a7a9c5eeeccf
shanybaruch <baruchshany@gmail.com>
2025-10-02 18:57:47 +0300

    Add btn add book

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -4,15 +4,19 @@ import { BookPreview } from './BookPreview.jsx';
 export function BookList({ books, onRemoveBook }) {
 
     return (
-        <ul className="book-list container">
-            {books.map(book =>
-                <li key={book.id}>
-                    {book.listPrice.isOnSale && <h4 className='sale'>Sale</h4>}
-                    <BookPreview book={book} />
-                    <button className="fa-regular fa-trash-can btn-remove" onClick={ev => onRemoveBook(book.id, ev)}></button>
-                    <button><Link to={`/book/${book.id}`}>details</Link></button>
-                </li>
-            )}
-        </ul>
+        <section>
+            <button className='btn-add-book'>Add Book</button>
+
+            <ul className="book-list container">
+                {books.map(book =>
+                    <li key={book.id}>
+                        {book.listPrice.isOnSale && <h4 className='sale'>Sale</h4>}
+                        <BookPreview book={book} />
+                        <button className="fa-regular fa-trash-can btn-remove" onClick={ev => onRemoveBook(book.id, ev)}></button>
+                        <button><Link to={`/book/${book.id}`}>details</Link></button>
+                    </li>
+                )}
+            </ul>
+        </section>
     )
 }
